4c10dcdd34d84c50e6755869ebbcfb2fd1193aa8
voilalex <ilya.vouk@gmail.com>
2020-12-05 17:35:27 +0300

    Tag, push, upload to PyPI

--- tag_push_upload.sh
+++ tag_push_upload.sh
@@ -6,9 +6,9 @@ git push
 VERSION=$(grep "version=" setup.py | grep -P "[\'\"].*[\'\"]" -o | grep -P "[^\'\"]*" -o) &&
 git tag v$VERSION &&
 git push origin v$VERSION &&
-python3 setup.py sdist &&
-python3 -m twine upload dist/* &&
-rm -rf dist &&
+python3 setup.py sdist
+python3 -m twine upload dist/*
+rm -rf dist
 rm -rf $(ls | grep ".*\.egg-info")
 
 
